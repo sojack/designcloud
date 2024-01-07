18b96174cda648d90ff37de044d92d7ca8bd793b
sojack <sojack@gmail.com>
2024-01-07 11:34:49 -0500

    updated description

--- app/page.js
+++ app/page.js
@@ -9,12 +9,13 @@ export default function Home() {
         <div>
           <Image src={dclogo} alt="design cloud logo" title="design cloud"/>
         </div>
-        <h1>Professional design at your fingertips</h1>
-        <p><a href="mailto:designcloud@jsdesigns.ca" title="email">Send us</a> your document and we beautify it.</p>
+        <h1>World-class design at your fingertips</h1>
+        <p><a href="mailto:designcloud@jsdesigns.ca" title="email">Send us</a> your document and we will turn it into a clear, beautiful, and accessible presentation.</p>
         <h2>Give it a try</h2>
         <p>We create documents for presentations, online, print, displays, etc.</p>
         <p>Turn your rough ideas into finished products.</p>
       </div>
+
     </main>
   )
 }
