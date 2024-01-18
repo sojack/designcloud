4c96ce8e89934ee28cef7bd8c6cb0a887a7199e0
sojack <sojack@gmail.com>
2024-01-17 22:45:59 -0500

    add emoji as favicon

--- app/vivian/page.js
+++ app/vivian/page.js
@@ -4,6 +4,9 @@ import "./page.css"
 export const metadata = {
     title: 'Vivian Resume',
     description: 'Vivian Shaw - Resume',
+    icons:{
+        icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌻</text></svg>'
+    }
     }
 
 
