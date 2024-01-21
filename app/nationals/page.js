74297ed69e76c080d8ddc2dfe83da35f782b0f98
sojack <sojack@gmail.com>
2024-01-21 10:16:44 -0800

    add nationals page

---
+++ app/nationals/page.js
@@ -0,0 +1,48 @@
+import data from './nationals.json'
+import './nationals.css'
+
+export const metadata = {
+    title: 'Canadian Championships'
+    }
+
+function page() {
+  return (
+    <div>
+        <h1>Canadian Masters Swimming Championships</h1>
+        <table>
+                <thead>
+                    <tr>
+                    <td></td>
+                    <td>Year</td>
+                    <td>Hosting City</td>
+                    <td>Participants</td>
+                    <td>Format</td>
+                    </tr>
+                </thead>
+            <tbody>
+                {
+                data.map(x=>
+                    <tr key={x.year}
+                        className={x.on==0?'canceled':''}
+                    >
+                        <td
+                        className={x.creator=='Betty'?'betty':'brigitte'}
+                        
+                        >•</td>
+                        <td>{x.year}</td>
+                        <td>{x.city}</td>
+                        <td>{x.entries}</td>
+                        <td>{x.format}</td>
+                    </tr>
+                    )
+                }
+
+            </tbody>
+        </table>
+        <p className='betty'>Betty Whital</p>
+        <p className='brigitte'>Brigitte Zirger</p>
+    </div>
+  )
+}
+
+export default page
