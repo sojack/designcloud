24d9307c96b18fec74cd0e8b9624ac5e286b8162
sojack <sojack@gmail.com>
2024-02-14 06:56:05 -0500

    add keeley page

--- app/keeley/page.js
+++ app/keeley/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image"
 import styles from "./keeley.module.css"
-import hydra from "./KeeleyHydra.jpg"
-import hydraLogo from "./logo-Hydra.svg"
-import keeleyLogo from "./logo-keeley-w.svg"
+import hydra from "./images/KeeleyHydra.jpg"
+import hydraLogo from "./images/logo-Hydra.svg"
+import keeleyLogo from "./images/logo-keeley-w.svg"
+import back from "./images/back-detail.png"
+import wire from "./images/wire.png"
 
 export const metadata = {
     title: 'Keeley Electronics - Hydra',
@@ -19,7 +21,15 @@ function page() {
             The HYDRA combines high-fidelity Reverb and Tremolo to add space and dimension to your guitar, bass, or keyboard signal. After the team at Keeley completed the ECCOS, we wanted to craft the perfect companion pedal, the tone of which had to make it impossible to turn off.
             </p>
             <Image src={keeleyLogo} alt="Keeley Hydra" title="Keeley Hydra" className={styles.logos}/>
+        </div>
+        <div className={styles.secondaryImagesContainer}>
+            <div className={styles.imageWrap}>
+                <Image src={wire} alt="Keeley Hydra" title="Keeley Hydra" className={styles.secondaryImages}/>
+            </div>
 
+            <div className={styles.imageWrap}>
+                <Image src={back} alt="Keeley Hydra" title="Keeley Hydra" className={styles.secondaryImages}/>
+            </div>
         </div>
     </div>
   )
